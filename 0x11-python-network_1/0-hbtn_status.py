ea87dac7aa6b932587328f991ee6d7b0f1891f72
RanaHany265 <you@ranahanymohamed265@gmail.com>
2024-03-28 23:48:12 +0000

    111

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,20 @@
 i!/usr/bin/python3
 """A script that
-- fetches https://alx-intranet.hbtn.io/status.
-- uses urlib package
+-  fetches https://alx-intranet.hbtn.io/status
+-   use the package urllib
+-    not allowed to import any packages other than urllib
 """
+def main():
+    url = "https://alx-intranet.hbtn.io/status"
 
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        utf8_content = content.decode('utf-8')
 
-if __name__ == '__main__':
-    import urllib.request
-
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        content = res.read()
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        print("\t- type:", type(content))
+        print("\t- content:", content)
+        print("\t- utf8 content:", utf8_content)
+
+if __name__ == "__main__":
+    main()
