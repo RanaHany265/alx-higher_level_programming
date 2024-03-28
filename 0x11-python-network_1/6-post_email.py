f90df2f6adff40e475088885efd7fd628195a6ce
RanaHany265 <you@ranahanymohamed265@gmail.com>
2024-03-28 23:29:32 +0000

    111

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """A script that:
-- takes in a URL,
-- sends a request to the URL and displays the value
-- of the X-Request-Id variable found in the header ofthe response.
+- takes in a URL and an email address,
+- sends a POST request to the passed URL with the email as a parameter,
+- displays the body of the response.
 """
 import sys
 import urllib.request
